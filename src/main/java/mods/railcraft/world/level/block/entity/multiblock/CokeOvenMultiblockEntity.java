a0ff5e75f3e863bf8e3e52c84b948ceff47eb790
LetterN <24603524+LetterN@users.noreply.github.com>
2021-10-10 19:43:23 +0800

    remove useless stuff

--- src/main/java/mods/railcraft/world/level/block/entity/multiblock/CokeOvenMultiblockEntity.java
+++ src/main/java/mods/railcraft/world/level/block/entity/multiblock/CokeOvenMultiblockEntity.java
@@ -30,7 +30,6 @@ import net.minecraft.tileentity.TileEntityType;
 import net.minecraft.util.IIntArray;
 import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.vector.Vector3i;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.fluids.FluidStack;
