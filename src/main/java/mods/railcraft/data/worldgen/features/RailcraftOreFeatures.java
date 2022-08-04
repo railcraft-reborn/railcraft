2b7f3b0c58419d1ac64c2fa0c4dfe08b0e7ab2b9
Davide Albiero <davide2910@live.it>
2022-08-04 16:58:22 +0200

    Ore gen, missing saltpeter

--- src/main/java/mods/railcraft/data/worldgen/features/RailcraftOreFeatures.java
+++ src/main/java/mods/railcraft/data/worldgen/features/RailcraftOreFeatures.java
@@ -19,7 +19,14 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class RailcraftOreFeatures {
 
-    private static final int LEAD_VEIN_DIMENSION = 7;
+    private static final int LEAD_VEIN_DIMENSION = 9;
+    private static final int TIN_SMALL_VEIN_DIMENSION = 4;
+    private static final int TIN_LARGE_VEIN_DIMENSION = 9;
+    private static final int SULFURE_VEIN_DIMENSION = 10;
+    private static final int ZINC_VEIN_DIMENSION = 6;
+    private static final int NICKEL_VEIN_DIMENSION = 7;
+    private static final int NICKEL_SMALL_VEIN_DIMENSION = 4;
+    private static final int SILVER_VEIN_DIMENSION = 10;
 
     private static final DeferredRegister<ConfiguredFeature<?, ?>> deferredRegister =
         DeferredRegister.create(Registry.CONFIGURED_FEATURE_REGISTRY, Railcraft.ID);
@@ -28,7 +35,7 @@ public class RailcraftOreFeatures {
         CONFIGURED_FEATURE_MAP = new HashMap<>();
 
 
-    private static final Supplier<List<OreConfiguration.TargetBlockState>> ORE_LEAD_TARGET_LIST =
+    private static final Supplier<List<OreConfiguration.TargetBlockState>> LEAD_ORE_TARGET_LIST =
         Suppliers.memoize(() -> List.of(
             OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES,
                 RailcraftBlocks.LEAD_ORE.get().defaultBlockState()),
@@ -36,8 +43,75 @@ public class RailcraftOreFeatures {
                 RailcraftBlocks.DEEPSLATE_LEAD_ORE.get().defaultBlockState())
         ));
 
-    public static final RegistryObject<ConfiguredFeature<?, ?>> ORE_LEAD = register("lead_ore",
-        () -> new OreConfiguration(ORE_LEAD_TARGET_LIST.get(), LEAD_VEIN_DIMENSION));
+    private static final Supplier<List<OreConfiguration.TargetBlockState>> TIN_ORE_TARGET_LIST =
+        Suppliers.memoize(() -> List.of(
+            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES,
+                RailcraftBlocks.TIN_ORE.get().defaultBlockState()),
+            OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES,
+                RailcraftBlocks.DEEPSLATE_TIN_ORE.get().defaultBlockState())
+        ));
+
+    private static final Supplier<List<OreConfiguration.TargetBlockState>> SULFURE_ORE_TARGET_LIST =
+        Suppliers.memoize(() -> List.of(
+            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES,
+                RailcraftBlocks.SULFURE_ORE.get().defaultBlockState()),
+            OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES,
+                RailcraftBlocks.DEEPSLATE_SULFURE_ORE.get().defaultBlockState())
+        ));
+
+    private static final Supplier<List<OreConfiguration.TargetBlockState>> ZINC_ORE_TARGET_LIST =
+        Suppliers.memoize(() -> List.of(
+            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES,
+                RailcraftBlocks.ZINC_ORE.get().defaultBlockState()),
+            OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES,
+                RailcraftBlocks.DEEPSLATE_ZINC_ORE.get().defaultBlockState())
+        ));
+
+    private static final Supplier<List<OreConfiguration.TargetBlockState>> NICKEL_ORE_TARGET_LIST =
+        Suppliers.memoize(() -> List.of(
+            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES,
+                RailcraftBlocks.NICKEL_ORE.get().defaultBlockState()),
+            OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES,
+                RailcraftBlocks.DEEPSLATE_NICKEL_ORE.get().defaultBlockState())
+        ));
+
+    private static final Supplier<List<OreConfiguration.TargetBlockState>> SILVER_ORE_TARGET_LIST =
+        Suppliers.memoize(() -> List.of(
+            OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES,
+                RailcraftBlocks.SILVER_ORE.get().defaultBlockState()),
+            OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES,
+                RailcraftBlocks.DEEPSLATE_SILVER_ORE.get().defaultBlockState())
+        ));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> LEAD_ORE = register("lead_ore",
+        () -> new OreConfiguration(LEAD_ORE_TARGET_LIST.get(), LEAD_VEIN_DIMENSION));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> TIN_ORE_SMALL = register("tin_ore_small",
+        () -> new OreConfiguration(TIN_ORE_TARGET_LIST.get(), TIN_SMALL_VEIN_DIMENSION));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> TIN_ORE_LARGE = register("tin_ore_large",
+        () -> new OreConfiguration(TIN_ORE_TARGET_LIST.get(), TIN_LARGE_VEIN_DIMENSION));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> SULFURE_ORE = register("sulfure_ore",
+        () -> new OreConfiguration(SULFURE_ORE_TARGET_LIST.get(), SULFURE_VEIN_DIMENSION));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> SULFURE_ORE_BURIED = register("sulfure_ore_buried",
+        () -> new OreConfiguration(SULFURE_ORE_TARGET_LIST.get(), SULFURE_VEIN_DIMENSION, 0.5F));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> ZINC_ORE = register("zinc_ore",
+        () -> new OreConfiguration(ZINC_ORE_TARGET_LIST.get(), ZINC_VEIN_DIMENSION));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> NICKEL_ORE = register("nickel_ore",
+        () -> new OreConfiguration(NICKEL_ORE_TARGET_LIST.get(), NICKEL_VEIN_DIMENSION));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> NICKEL_ORE_SMALL = register("nickel_ore_small",
+        () -> new OreConfiguration(NICKEL_ORE_TARGET_LIST.get(), NICKEL_SMALL_VEIN_DIMENSION));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> SILVER_ORE = register("silver_ore",
+        () -> new OreConfiguration(SILVER_ORE_TARGET_LIST.get(), SILVER_VEIN_DIMENSION));
+
+    public static final RegistryObject<ConfiguredFeature<?, ?>> SILVER_ORE_BURIED = register("silver_ore_buried",
+        () -> new OreConfiguration(SILVER_ORE_TARGET_LIST.get(), SILVER_VEIN_DIMENSION, 0.5F));
 
 
     private static RegistryObject<ConfiguredFeature<?, ?>> register(String name,
--- src/main/java/mods/railcraft/data/worldgen/placements/RailcraftOrePlacements.java
+++ src/main/java/mods/railcraft/data/worldgen/placements/RailcraftOrePlacements.java
@@ -1,6 +1,7 @@
 package mods.railcraft.data.worldgen.placements;
 
 import static net.minecraft.data.worldgen.placement.OrePlacements.commonOrePlacement;
+import static net.minecraft.data.worldgen.placement.OrePlacements.orePlacement;
 
 import java.util.HashMap;
 import java.util.List;
@@ -11,8 +12,10 @@ import mods.railcraft.data.worldgen.features.RailcraftOreFeatures;
 import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.placement.CountPlacement;
 import net.minecraft.world.level.levelgen.placement.HeightRangePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 import net.minecraft.world.level.levelgen.placement.PlacementModifier;
@@ -22,7 +25,16 @@ import net.minecraftforge.registries.RegistryObject;
 
 public class RailcraftOrePlacements {
 
-    private static final int LEAD_VEIN_PER_CHUNCK = 10;
+    private static final int LEAD_VEIN_PER_CHUNK = 8;
+    private static final int TIN_SMALL_VEIN_PER_CHUNK = 14;
+    private static final int TIN_LARGE_VEIN_PER_CHUNK = 12;
+    private static final int SULFURE_VEIN_PER_CHUNK = 20;
+    private static final int SULFURE_BURIED_VEIN_PER_CHUNK = 12;
+    private static final int ZINC_VEIN_PER_CHUNK = 7;
+    private static final int NICKEL_UPPER_VEIN_PER_CHUNK = 70;
+    private static final int NICKEL_MIDDLE_VEIN_PER_CHUNK = 10;
+    private static final int NICKEL_SMALL_VEIN_PER_CHUNK = 10;
+    private static final int SILVER_VEIN_PER_CHUNK = 6;
 
     private static final DeferredRegister<PlacedFeature> deferredRegister =
         DeferredRegister.create(Registry.PLACED_FEATURE_REGISTRY, Railcraft.ID);
@@ -30,9 +42,56 @@ public class RailcraftOrePlacements {
     private static final Map<ResourceLocation, RegistryObject<PlacedFeature>>
         PLACED_FEATURE_MAP = new HashMap<>();
 
-    public static final RegistryObject<PlacedFeature> ORE_LEAD_MIDDLE = register("ore_lead_middle",
-        RailcraftOreFeatures.ORE_LEAD, () -> commonOrePlacement(LEAD_VEIN_PER_CHUNCK,
+    public static final RegistryObject<PlacedFeature> LEAD_ORE = register("lead_ore",
+        RailcraftOreFeatures.LEAD_ORE, () -> commonOrePlacement(LEAD_VEIN_PER_CHUNK,
+            HeightRangePlacement.triangle(VerticalAnchor.absolute(-24), VerticalAnchor.absolute(64))
+        ));
+
+    public static final RegistryObject<PlacedFeature> TIN_ORE_SMALL = register("tin_ore_small",
+        RailcraftOreFeatures.TIN_ORE_SMALL, () -> commonOrePlacement(TIN_SMALL_VEIN_PER_CHUNK,
+            HeightRangePlacement.triangle(VerticalAnchor.absolute(-20), VerticalAnchor.absolute(94))
+        ));
+    public static final RegistryObject<PlacedFeature> TIN_ORE_LARGE = register("tin_ore_large",
+        RailcraftOreFeatures.TIN_ORE_LARGE, () -> commonOrePlacement(TIN_LARGE_VEIN_PER_CHUNK,
+            HeightRangePlacement.triangle(VerticalAnchor.absolute(-32), VerticalAnchor.absolute(72))
+        ));
+
+    public static final RegistryObject<PlacedFeature> SULFURE_ORE_UPPER = register("sulfure_ore_upper",
+        RailcraftOreFeatures.SULFURE_ORE, () -> commonOrePlacement(SULFURE_VEIN_PER_CHUNK,
+            HeightRangePlacement.uniform(VerticalAnchor.absolute(136), VerticalAnchor.top())
+        ));
+    public static final RegistryObject<PlacedFeature> SULFURE_ORE_LOWER = register("sulfure_ore_lower",
+        RailcraftOreFeatures.SULFURE_ORE_BURIED, () -> commonOrePlacement(SULFURE_BURIED_VEIN_PER_CHUNK,
+            HeightRangePlacement.triangle(VerticalAnchor.absolute(-10), VerticalAnchor.absolute(182))
+        ));
+
+    public static final RegistryObject<PlacedFeature> ZINC_ORE = register("zinc_ore",
+        RailcraftOreFeatures.ZINC_ORE, () -> commonOrePlacement(ZINC_VEIN_PER_CHUNK,
+            HeightRangePlacement.triangle(VerticalAnchor.absolute(-24), VerticalAnchor.absolute(64))
+        ));
+
+    public static final RegistryObject<PlacedFeature> NICKEL_ORE_UPPER = register("nickel_ore_upper",
+        RailcraftOreFeatures.NICKEL_ORE, () -> commonOrePlacement(NICKEL_UPPER_VEIN_PER_CHUNK,
+            HeightRangePlacement.triangle(VerticalAnchor.absolute(80), VerticalAnchor.absolute(384))
+        ));
+    public static final RegistryObject<PlacedFeature> NICKEL_ORE_MIDDLE = register("nickel_ore_middle",
+        RailcraftOreFeatures.NICKEL_ORE, () -> commonOrePlacement(NICKEL_MIDDLE_VEIN_PER_CHUNK,
             HeightRangePlacement.triangle(VerticalAnchor.absolute(-24), VerticalAnchor.absolute(56))
+        ));
+    public static final RegistryObject<PlacedFeature> NICKEL_ORE_SMALL = register("nickel_ore_small",
+        RailcraftOreFeatures.NICKEL_ORE_SMALL, () -> commonOrePlacement(NICKEL_SMALL_VEIN_PER_CHUNK,
+            HeightRangePlacement.uniform(VerticalAnchor.bottom(), VerticalAnchor.absolute(72))
+        ));
+
+
+    public static final RegistryObject<PlacedFeature> SILVER_ORE = register("silver_ore",
+        RailcraftOreFeatures.SILVER_ORE_BURIED, () -> commonOrePlacement(SILVER_VEIN_PER_CHUNK,
+            HeightRangePlacement.triangle(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(32))
+        ));
+    public static final RegistryObject<PlacedFeature> SILVER_ORE_LOWER = register("silver_ore_lower",
+        RailcraftOreFeatures.SILVER_ORE, () -> orePlacement(
+            CountPlacement.of(UniformInt.of(0, 1)),
+            HeightRangePlacement.uniform(VerticalAnchor.absolute(-64), VerticalAnchor.absolute(-48))
         ));
 
     private static RegistryObject<PlacedFeature> register(String name,
