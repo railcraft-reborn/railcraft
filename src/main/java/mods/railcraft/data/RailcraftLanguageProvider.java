fbdd23715ebcf11fd602f3511cc925aed0b15077
Davide Albiero <davide2910@live.it>
2022-09-01 12:53:54 +0200

    Fix translation

--- src/main/java/mods/railcraft/data/RailcraftLanguageProvider.java
+++ src/main/java/mods/railcraft/data/RailcraftLanguageProvider.java
@@ -323,7 +323,7 @@ public class RailcraftLanguageProvider extends LanguageProvider {
     this.add(Tips.CRAWBAR_SEASON_DEFAULT, "Default");
     this.add(Tips.CRAWBAR_SEASON_CHRISTMAS, "Christmas");
     this.add(Tips.CRAWBAR_SEASON_HALLOWEEN, "Halloween");
-    this.add(Tips.SIGNAL_LABEL_DESC1, "Rename in Anvil");
+    this.add(Tips.SIGNAL_LABEL_DESC1, "- Rename in Anvil -");
     this.add(Tips.SIGNAL_LABEL_DESC2, "Right click while sneaking to name a signal or signal box");
     this.add(Tips.SENDS_SIGNALS_TO_RECEIVERS, "Sends signals to Receivers");
     this.add(Tips.SIGNAL_RECEIVER_BOX, "Listens to Controller signals");
