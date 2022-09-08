5600dcc7dbbd94bd89ecad35cc18c7cec24a8bfa
Joseph Tarbit <joetarbit@gmail.com>
2022-09-08 14:42:44 +0100

    Don't use Optional as field

--- src/main/java/mods/railcraft/world/module/CookingModule.java
+++ src/main/java/mods/railcraft/world/module/CookingModule.java
@@ -1,25 +1,23 @@
 package mods.railcraft.world.module;
 
-import java.util.Optional;
+import java.util.Objects;
 import javax.annotation.OverridingMethodsMustInvokeSuper;
-import net.minecraft.world.SimpleContainer;
+import org.jetbrains.annotations.Nullable;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.AbstractCookingRecipe;
 import net.minecraft.world.item.crafting.RecipeType;
-import org.jetbrains.annotations.Nullable;
 
 public abstract class CookingModule<R extends AbstractCookingRecipe, T extends ModuleProvider>
     extends CrafterModule<T> {
 
   private final int inputSlot;
   @Nullable
-  protected Optional<R> recipe;
+  protected R recipe;
   private ItemStack lastInput = ItemStack.EMPTY;
 
   protected CookingModule(T provider, int size, int inputSlot) {
     super(provider, size);
     this.inputSlot = inputSlot;
-    this.recipe = Optional.empty();
   }
 
   protected abstract RecipeType<R> getRecipeType();
@@ -30,8 +28,10 @@ public abstract class CookingModule<R extends AbstractCookingRecipe, T extends M
     var input = this.getItem(this.inputSlot);
     if (!ItemStack.matches(input, this.lastInput)) {
       this.lastInput = input.copy();
-      this.recipe = getRecipe(input);
-      if (this.recipe.isEmpty() && !input.isEmpty()) {
+      this.recipe = this.provider.level().getRecipeManager()
+          .getRecipeFor(this.getRecipeType(), this, this.provider.level())
+          .orElse(null);
+      if (this.recipe == null && !input.isEmpty()) {
         this.setItem(this.inputSlot, ItemStack.EMPTY);
         this.provider.dropItem(input);
       }
@@ -40,19 +40,12 @@ public abstract class CookingModule<R extends AbstractCookingRecipe, T extends M
 
   @Override
   protected boolean lacksRequirements() {
-    return this.recipe.isEmpty();
+    return this.recipe == null;
   }
 
   @Override
   protected final int calculateDuration() {
-    return this.recipe
-        .map(AbstractCookingRecipe::getCookingTime)
-        .orElseThrow(NullPointerException::new);
-  }
-
-  protected Optional<R> getRecipe(ItemStack itemStack) {
-    return provider.level().getRecipeManager()
-        .getRecipeFor(this.getRecipeType(),
-            new SimpleContainer(itemStack), this.provider.level());
+    Objects.requireNonNull(this.recipe);
+    return this.recipe.getCookingTime();
   }
 }
