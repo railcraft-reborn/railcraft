aa0c3accb219708087de47e7a44eb996c2a724a7
Joseph Tarbit <joetarbit@gmail.com>
2022-09-15 16:04:46 +0100

    Save emitter for force track block

--- src/main/java/mods/railcraft/world/level/block/entity/track/ForceTrackBlockEntity.java
+++ src/main/java/mods/railcraft/world/level/block/entity/track/ForceTrackBlockEntity.java
@@ -1,42 +1,61 @@
 package mods.railcraft.world.level.block.entity.track;
 
+import org.jetbrains.annotations.Nullable;
 import mods.railcraft.world.level.block.ForceTrackEmitterBlock;
 import mods.railcraft.world.level.block.entity.ForceTrackEmitterBlockEntity;
 import mods.railcraft.world.level.block.entity.RailcraftBlockEntity;
 import mods.railcraft.world.level.block.entity.RailcraftBlockEntityTypes;
 import mods.railcraft.world.level.block.track.ForceTrackBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.NbtUtils;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jetbrains.annotations.Nullable;
 
 public final class ForceTrackBlockEntity extends RailcraftBlockEntity {
 
-  @Nullable
   private ForceTrackEmitterBlockEntity emitter;
+  @Nullable
+  private BlockPos emitterPos;
 
   public ForceTrackBlockEntity(BlockPos blockPos, BlockState blockState) {
     super(RailcraftBlockEntityTypes.FORCE_TRACK.get(), blockPos, blockState);
   }
 
   public void blockRemoved() {
-    if (this.emitter != null) {
-      this.emitter.clearTracks(this.getBlockPos());
-    }
+    this.emitter.clearTracks(this.getBlockPos());
   }
 
   public void neighborChanged() {
-    if (this.emitter != null) {
-      this.emitter.notifyTrackChange();
-    }
+    this.emitter.notifyTrackChange();
   }
 
-  public void setEmitter(@Nullable ForceTrackEmitterBlockEntity emitter) {
+  public void setEmitter(ForceTrackEmitterBlockEntity emitter) {
     this.emitter = emitter;
-    if (emitter != null) {
-      this.setOwner(emitter.getOwner().orElse(null));
-      this.level.setBlockAndUpdate(this.getBlockPos(),
-          this.getBlockState().setValue(ForceTrackBlock.COLOR,
-              emitter.getBlockState().getValue(ForceTrackEmitterBlock.COLOR)));
-    }
+    this.emitterPos = emitter.getBlockPos();
+    this.setOwner(emitter.getOwner().orElse(null));
+    this.level.setBlockAndUpdate(this.getBlockPos(),
+        this.getBlockState().setValue(ForceTrackBlock.COLOR,
+            emitter.getBlockState().getValue(ForceTrackEmitterBlock.COLOR)));
+  }
+
+  @Override
+  public void onLoad() {
+    super.onLoad();
+    this.emitter = this.emitterPos == null
+        ? null
+        : this.level
+            .getBlockEntity(this.emitterPos, RailcraftBlockEntityTypes.FORCE_TRACK_EMITTER.get())
+            .orElse(null);
+  }
+
+  @Override
+  protected void saveAdditional(CompoundTag tag) {
+    super.saveAdditional(tag);
+    tag.put("emitterPos", NbtUtils.writeBlockPos(this.emitterPos));
+  }
+
+  @Override
+  public void load(CompoundTag tag) {
+    this.emitterPos = NbtUtils.readBlockPos(tag.getCompound("emitterPos"));
   }
 }
