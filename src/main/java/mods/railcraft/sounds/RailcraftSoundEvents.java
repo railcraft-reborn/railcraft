2a9df41e426fc50b2b078e6be34804f434b0c2bf
Joseph Tarbit <joetarbit@gmail.com>
2022-12-12 14:23:49 +0000

    Sounds

--- src/main/java/mods/railcraft/sounds/RailcraftSoundEvents.java
+++ src/main/java/mods/railcraft/sounds/RailcraftSoundEvents.java
@@ -1,5 +1,6 @@
 package mods.railcraft.sounds;
 
+import java.util.function.Function;
 import mods.railcraft.Railcraft;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
@@ -14,23 +15,27 @@ public class RailcraftSoundEvents {
       DeferredRegister.create(ForgeRegistries.SOUND_EVENTS, Railcraft.ID);
 
   public static final RegistryObject<SoundEvent> STEAM_WHISTLE =
-      register("locomotive.steam.whistle");
+      register("locomotive.steam.whistle", SoundEvent::createVariableRangeEvent);
 
   public static final RegistryObject<SoundEvent> ELECTRIC_WHISTLE =
-      register("locomotive.electric.whistle");
+      register("locomotive.electric.whistle", SoundEvent::createVariableRangeEvent);
 
-  public static final RegistryObject<SoundEvent> STEAM_BURST = register("machine.steam_burst");
+  public static final RegistryObject<SoundEvent> STEAM_BURST =
+      register("machine.steam_burst", SoundEvent::createVariableRangeEvent);
 
-  public static final RegistryObject<SoundEvent> STEAM_HISS = register("machine.steam_hiss");
+  public static final RegistryObject<SoundEvent> STEAM_HISS =
+      register("machine.steam_hiss", SoundEvent::createVariableRangeEvent);
 
-  public static final RegistryObject<SoundEvent> MACHINE_ZAP = register("machine.zap");
+  public static final RegistryObject<SoundEvent> MACHINE_ZAP =
+      register("machine.zap", SoundEvent::createVariableRangeEvent);
 
   public static void register(IEventBus modEventBus) {
     deferredRegister.register(modEventBus);
   }
 
-  private static RegistryObject<SoundEvent> register(String name) {
-    ResourceLocation registryName = new ResourceLocation(Railcraft.ID, name);
-    return deferredRegister.register(name, () -> new SoundEvent(registryName));
+  private static RegistryObject<SoundEvent> register(String name,
+      Function<ResourceLocation, SoundEvent> factory) {
+    var registryName = new ResourceLocation(Railcraft.ID, name);
+    return deferredRegister.register(name, () -> factory.apply(registryName));
   }
 }
