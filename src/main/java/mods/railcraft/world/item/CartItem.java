ae7560a5a08897f2151e952ef4c616ba3c6c2b7d
Davide Albiero <davide2910@live.it>
2023-01-31 18:46:41 +0100

    Adjusted the placement of the carts

--- src/main/java/mods/railcraft/world/item/CartItem.java
+++ src/main/java/mods/railcraft/world/item/CartItem.java
@@ -6,6 +6,7 @@ import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.block.BaseRailBlock;
+import net.minecraft.world.level.block.state.properties.RailShape;
 
 public class CartItem extends Item {
 
@@ -21,14 +22,21 @@ public class CartItem extends Item {
     var player = context.getPlayer();
     var hand = context.getHand();
     var level = context.getLevel();
-    var pos = context.getClickedPos();
+    var railPos = context.getClickedPos();
+    var railState = level.getBlockState(railPos);
     var itemStack = player.getItemInHand(hand);
-    if (!BaseRailBlock.isRail(level, pos)) {
+    if (!BaseRailBlock.isRail(level, railPos)) {
       return InteractionResult.FAIL;
     }
     if (level instanceof ServerLevel serverLevel) {
-      var minecart = this.minecartFactory.createMinecart(
-          itemStack, pos.getX(), pos.getY(), pos.getZ(), serverLevel);
+      var railShape = RailShape.NORTH_SOUTH;
+      if (railState.getBlock() instanceof BaseRailBlock baseRailBlock) {
+        railShape = baseRailBlock.getRailDirection(railState, level, railPos, null);
+      }
+      double d0 = railShape.isAscending() ? 0.5 : 0.0;
+
+      var minecart = this.minecartFactory.createMinecart(itemStack,
+          railPos.getX() + 0.5, railPos.getY() + d0 + 0.0625, railPos.getZ() + 0.5, serverLevel);
       if (minecart != null) {
         minecart.setYRot(context.getHorizontalDirection().toYRot());
         level.addFreshEntity(minecart);
