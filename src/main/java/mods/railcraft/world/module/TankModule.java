74d7706cdcce69920baae46358e5493a6f9f2edb
Davide Albiero <davide2910@live.it>
2023-03-10 11:14:29 +0100

    Fixed container problem

--- src/main/java/mods/railcraft/world/module/TankModule.java
+++ src/main/java/mods/railcraft/world/module/TankModule.java
@@ -17,11 +17,6 @@ import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.wrapper.InvWrapper;
 
-/**
- * Created by CovertJaguar on 1/28/2019 for Railcraft.
- *
- * @author CovertJaguar <http://www.railcraft.info>
- */
 public class TankModule extends ContainerModule<TankBlockEntity> {
 
   public static final int TANK_INDEX = 0;
@@ -71,15 +66,13 @@ public class TankModule extends ContainerModule<TankBlockEntity> {
 
   @Override
   public boolean canPlaceItem(int slot, ItemStack stack) {
-    if (!super.canPlaceItem(slot, stack)) {
-      return false;
-    }
-    if (slot == SLOT_INPUT) {
-      return (!this.tank.isEmpty()
+    return switch (slot) {
+      case SLOT_INPUT -> (!this.tank.isEmpty()
           && FluidItemHelper.isRoomInContainer(stack, this.tank.getFluidType()))
           || FluidUtil.getFluidContained(stack).isPresent();
-    }
-    return false;
+      case SLOT_PROCESS, SLOT_OUTPUT -> true;
+      default -> false;
+    } && super.canPlaceItem(slot, stack);
   }
 
   public LazyOptional<IItemHandler> getItemHandler() {
