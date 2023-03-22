06dfa7f50910343927745e161b705e872c1c2d48
Davide Albiero <davide2910@live.it>
2023-03-22 21:12:54 +0100

    Fix #49

--- src/main/java/mods/railcraft/client/util/FluidRenderer.java
+++ src/main/java/mods/railcraft/client/util/FluidRenderer.java
@@ -1,25 +1,20 @@
 package mods.railcraft.client.util;
 
+import org.jetbrains.annotations.NotNull;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.inventory.InventoryMenu;
-import net.minecraft.world.level.material.Fluid;
 import net.minecraftforge.client.extensions.common.IClientFluidTypeExtensions;
 import net.minecraftforge.fluids.FluidStack;
 
-/**
- * @author CovertJaguar <https://www.railcraft.info>
- */
 public class FluidRenderer {
 
-  private static final Minecraft minecraft = Minecraft.getInstance();
-
+  @NotNull
   public static CuboidModel getFluidModel(FluidStack fluid, float maxX, float maxY, float maxZ,
       FluidType type) {
     var model = new CuboidModel();
-    model.setAll(model.new Face().setSprite(
-        FluidRenderer.getFluidTexture(fluid, type)));
+    model.setAll(model.new Face().setSprite(FluidRenderer.getFluidTexture(fluid, type)));
 
     model.setMinX(0.0F);
     model.setMinY(0.0F);
@@ -32,16 +27,15 @@ public class FluidRenderer {
     return model;
   }
 
-  public static TextureAtlasSprite getFluidTexture(FluidStack fluidStack,
-      FluidType type) {
-    Fluid fluid = fluidStack.getFluid();
+  public static TextureAtlasSprite getFluidTexture(FluidStack fluidStack, FluidType type) {
+    var fluid = fluidStack.getFluid();
     ResourceLocation spriteLocation;
     if (type == FluidType.STILL) {
       spriteLocation = IClientFluidTypeExtensions.of(fluid).getStillTexture(fluidStack);
     } else {
       spriteLocation = IClientFluidTypeExtensions.of(fluid).getFlowingTexture(fluidStack);
     }
-    return minecraft.getTextureAtlas(InventoryMenu.BLOCK_ATLAS).apply(spriteLocation);
+    return Minecraft.getInstance().getTextureAtlas(InventoryMenu.BLOCK_ATLAS).apply(spriteLocation);
   }
 
   public enum FluidType {
