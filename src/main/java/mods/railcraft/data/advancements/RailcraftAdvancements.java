1f6190e600c44842e033b128a291b0208f82059b
Davide Albiero <davide2910@live.it>
2023-09-14 09:52:25 +0200

    Added more structures

---
+++ src/main/java/mods/railcraft/data/advancements/RailcraftAdvancements.java
@@ -0,0 +1,24 @@
+package mods.railcraft.data.advancements;
+
+import java.util.function.Consumer;
+import mods.railcraft.Railcraft;
+import mods.railcraft.data.loot.packs.RailcraftAdvancementRewardLoot;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.AdvancementRewards;
+import net.minecraft.advancements.critereon.PlayerTrigger;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.common.data.ForgeAdvancementProvider;
+
+class RailcraftAdvancements implements ForgeAdvancementProvider.AdvancementGenerator {
+
+  @Override
+  public void generate(HolderLookup.Provider registries, Consumer<Advancement> consumer,
+      ExistingFileHelper fileHelper) {
+    Advancement.Builder.advancement()
+        .addCriterion("tick", PlayerTrigger.TriggerInstance.tick())
+        .rewards(AdvancementRewards.Builder.loot(RailcraftAdvancementRewardLoot.PATCHOULI_BOOK))
+        .save(consumer, new ResourceLocation(Railcraft.ID, "grant_book_on_first_join"), fileHelper);
+  }
+}
--- src/main/java/mods/railcraft/integrations/patchouli/Patchouli.java
+++ src/main/java/mods/railcraft/integrations/patchouli/Patchouli.java
@@ -3,7 +3,8 @@ package mods.railcraft.integrations.patchouli;
 import mods.railcraft.Railcraft;
 import mods.railcraft.world.level.block.RailcraftBlocks;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.Block;
+import vazkii.patchouli.api.IStateMatcher;
 import vazkii.patchouli.api.PatchouliAPI;
 
 public class Patchouli {
@@ -11,19 +12,63 @@ public class Patchouli {
   public static void setup() {
     var patchouliApi = PatchouliAPI.get();
 
-    var crusherBlock = patchouliApi.predicateMatcher(RailcraftBlocks.CRUSHER.get(),
-        Patchouli::validEdge);
-
+    var crusherBlock = validBlock(patchouliApi, RailcraftBlocks.CRUSHER.get());
     var crusher = patchouliApi.makeMultiblock(new String[][]{
-            {"CC", "CC", "CC"}, //Y:1
-            {"CC", "0C", "CC"}  //Y:0
-        },
-        'C', crusherBlock, '0', crusherBlock
+        {"BB", "BB", "BB"}, //Y:1
+        {"BB", "0B", "BB"}  //Y:0
+        }, 'B', crusherBlock, '0', crusherBlock
     ).setSymmetrical(false);
     patchouliApi.registerMultiblock(new ResourceLocation(Railcraft.ID, "crusher"), crusher);
+
+    var cokeOvenBlock = validBlock(patchouliApi, RailcraftBlocks.COKE_OVEN_BRICKS.get());
+    var cokeOven = patchouliApi.makeMultiblock(new String[][]{
+        {"BBB", "BBB", "BBB"}, //Y:2
+        {"BBB", "BAB", "BBB"}, //Y:1
+        {"BBB", "B0B", "BBB"}  //Y:0
+        }, 'B', cokeOvenBlock, '0', cokeOvenBlock, 'A', patchouliApi.airMatcher()
+    ).setSymmetrical(true);
+    patchouliApi.registerMultiblock(new ResourceLocation(Railcraft.ID, "coke_oven"), cokeOven);
+
+    var blastFurnaceBlock = validBlock(patchouliApi, RailcraftBlocks.BLAST_FURNACE_BRICKS.get());
+    var blastFurnace = patchouliApi.makeMultiblock(new String[][]{
+        {"BBB", "BBB", "BBB"}, //Y:3
+        {"BBB", "BAB", "BBB"}, //Y:2
+        {"BBB", "BAB", "BBB"}, //Y:1
+        {"BBB", "B0B", "BBB"}  //Y:0
+        }, 'B', blastFurnaceBlock, '0', blastFurnaceBlock, 'A', patchouliApi.airMatcher()
+    ).setSymmetrical(true);
+    patchouliApi
+        .registerMultiblock(new ResourceLocation(Railcraft.ID, "blast_furnace"), blastFurnace);
+
+    var steamTurbineBlock = validBlock(patchouliApi, RailcraftBlocks.STEAM_TURBINE.get());
+    var steamTurbine = patchouliApi.makeMultiblock(new String[][]{
+            {"BB", "BB", "BB"}, //Y:1
+            {"BB", "0B", "BB"}  //Y:0
+        }, 'B', steamTurbineBlock, '0', steamTurbineBlock
+    ).setSymmetrical(false);
+    patchouliApi
+        .registerMultiblock(new ResourceLocation(Railcraft.ID, "steam_turbine"), steamTurbine);
+
+    var waterTankBlock = validBlock(patchouliApi, RailcraftBlocks.WATER_TANK_SIDING.get());
+    var waterTank = patchouliApi.makeMultiblock(new String[][]{
+            {"BB", "BB"}, //Y:1
+            {"BB", "0B"}  //Y:0
+        }, 'B', waterTankBlock, '0', waterTankBlock
+    ).setSymmetrical(true);
+    patchouliApi
+        .registerMultiblock(new ResourceLocation(Railcraft.ID, "water_tank"), waterTank);
+
+    var steamOvenBlock = validBlock(patchouliApi, RailcraftBlocks.STEAM_OVEN.get());
+    var steamOven = patchouliApi.makeMultiblock(new String[][]{
+            {"BB", "BB"}, //Y:1
+            {"BB", "0B"}  //Y:0
+        }, 'B', steamOvenBlock, '0', steamOvenBlock
+    ).setSymmetrical(true);
+    patchouliApi
+        .registerMultiblock(new ResourceLocation(Railcraft.ID, "steam_oven"), steamOven);
   }
 
-  private static boolean validEdge(BlockState state) {
-    return state.is(RailcraftBlocks.CRUSHER.get());
+  private static IStateMatcher validBlock(PatchouliAPI.IPatchouliAPI api, Block block) {
+    return api.predicateMatcher(block, state -> state.is(block));
   }
 }
