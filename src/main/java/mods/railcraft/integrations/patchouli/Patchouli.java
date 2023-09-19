3d5b73ed08972a9f9c6a823abeb97e6ca5a9a61e
Davide Albiero <davide2910@live.it>
2023-09-19 21:05:49 +0200

    Iron tank 5x5x5

--- src/main/java/mods/railcraft/integrations/patchouli/Patchouli.java
+++ src/main/java/mods/railcraft/integrations/patchouli/Patchouli.java
@@ -1,8 +1,13 @@
 package mods.railcraft.integrations.patchouli;
 
 import mods.railcraft.Railcraft;
+import mods.railcraft.tags.RailcraftTags;
 import mods.railcraft.world.level.block.RailcraftBlocks;
+import mods.railcraft.world.level.block.tank.IronTankGaugeBlock;
+import mods.railcraft.world.level.block.tank.IronTankValveBlock;
+import mods.railcraft.world.level.block.tank.IronTankWallBlock;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.Block;
 import vazkii.patchouli.api.IStateMatcher;
 import vazkii.patchouli.api.PatchouliAPI;
@@ -66,9 +71,46 @@ public class Patchouli {
     ).setSymmetrical(true);
     patchouliApi
         .registerMultiblock(new ResourceLocation(Railcraft.ID, "steam_oven"), steamOven);
+
+    var ironTankWallBlock = validTankWallBlock(patchouliApi,
+        RailcraftBlocks.IRON_TANK_WALL.variantFor(DyeColor.WHITE).get());
+    var ironTankGaugeBlock = validTankGaugeBlock(patchouliApi,
+        RailcraftBlocks.IRON_TANK_GAUGE.variantFor(DyeColor.WHITE).get());
+    var ironTankValveBlock = validTankValveBlock(patchouliApi,
+        RailcraftBlocks.IRON_TANK_VALVE.variantFor(DyeColor.WHITE).get());
+    var tank = patchouliApi.makeMultiblock(new String[][]{
+        {"BBBBB", "BBBBB", "BBVBB", "BBBBB", "BBBBB"}, //Y:4
+        {"BGGGB", "GAAAG", "GAAAG", "GAAAG", "BGGGB"}, //Y:3
+        {"BGGGB", "GAAAG", "GAAAG", "GAAAG", "BGGGB"}, //Y:2
+        {"BGGGB", "GAAAG", "GAAAG", "GAAAG", "BGGGB"}, //Y:1
+        {"BBBBB", "BBBBB", "BBVBB", "BBBBB", "BB0BB"}  //Y:0
+        },
+        'A', patchouliApi.airMatcher(),
+        'B', ironTankWallBlock,
+        'G', ironTankGaugeBlock,
+        'V', ironTankValveBlock,
+        '0', ironTankWallBlock
+    ).setSymmetrical(true);
+    patchouliApi
+        .registerMultiblock(new ResourceLocation(Railcraft.ID, "iron_tank"), tank);
   }
 
   private static IStateMatcher validBlock(PatchouliAPI.IPatchouliAPI api, Block block) {
     return api.predicateMatcher(block, state -> state.is(block));
+  }
+
+  private static IStateMatcher validTankWallBlock(PatchouliAPI.IPatchouliAPI api,
+      IronTankWallBlock block) {
+    return api.predicateMatcher(block, state -> state.is(RailcraftTags.Blocks.IRON_TANK_WALL));
+  }
+
+  private static IStateMatcher validTankGaugeBlock(PatchouliAPI.IPatchouliAPI api,
+      IronTankGaugeBlock block) {
+    return api.predicateMatcher(block, state -> state.is(RailcraftTags.Blocks.IRON_TANK_GAUGE));
+  }
+
+  private static IStateMatcher validTankValveBlock(PatchouliAPI.IPatchouliAPI api,
+      IronTankValveBlock block) {
+    return api.predicateMatcher(block, state -> state.is(RailcraftTags.Blocks.IRON_TANK_VALVE));
   }
 }
