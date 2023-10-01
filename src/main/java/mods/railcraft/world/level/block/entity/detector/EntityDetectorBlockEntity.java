21949cc5fd48d06b7ad3242db941ec5544f27f64
Davide Albiero <davide2910@live.it>
2023-10-01 16:15:22 +0200

    Created an abstract class

---
+++ src/main/java/mods/railcraft/world/level/block/entity/detector/EntityDetectorBlockEntity.java
@@ -0,0 +1,30 @@
+package mods.railcraft.world.level.block.entity.detector;
+
+import java.util.List;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.vehicle.AbstractMinecart;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.redstone.Redstone;
+
+public abstract class EntityDetectorBlockEntity<T extends Entity> extends DetectorBlockEntity {
+
+  private final Class<T> entityClass;
+
+  public EntityDetectorBlockEntity(BlockEntityType<?> type,
+      BlockPos blockPos, BlockState blockState, Class<T> entityClass) {
+    super(type, blockPos, blockState);
+    this.entityClass = entityClass;
+  }
+
+  @Override
+  protected int testCarts(List<AbstractMinecart> minecarts) {
+    for (var cart : minecarts) {
+      if (cart.getPassengers().stream().anyMatch(entityClass::isInstance)) {
+        return Redstone.SIGNAL_MAX;
+      }
+    }
+    return Redstone.SIGNAL_NONE;
+  }
+}
--- src/main/java/mods/railcraft/world/level/block/entity/detector/MobDetectorBlockEntity.java
+++ src/main/java/mods/railcraft/world/level/block/entity/detector/MobDetectorBlockEntity.java
@@ -1,26 +1,13 @@
 package mods.railcraft.world.level.block.entity.detector;
 
-import java.util.List;
 import mods.railcraft.world.level.block.entity.RailcraftBlockEntityTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.Mob;
-import net.minecraft.world.entity.vehicle.AbstractMinecart;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.redstone.Redstone;
 
-public class MobDetectorBlockEntity extends DetectorBlockEntity {
+public class MobDetectorBlockEntity extends EntityDetectorBlockEntity<Mob> {
 
   public MobDetectorBlockEntity(BlockPos blockPos, BlockState blockState) {
-    super(RailcraftBlockEntityTypes.MOB_DETECTOR.get(), blockPos, blockState);
-  }
-
-  @Override
-  protected int testCarts(List<AbstractMinecart> minecarts) {
-    for (var cart : minecarts) {
-      if (cart.getPassengers().stream().anyMatch(Mob.class::isInstance)) {
-        return Redstone.SIGNAL_MAX;
-      }
-    }
-    return Redstone.SIGNAL_NONE;
+    super(RailcraftBlockEntityTypes.MOB_DETECTOR.get(), blockPos, blockState, Mob.class);
   }
 }
