9a3814e46755947a0823713e0abc0a768179808e
Davide Albiero <davide2910@live.it>
2023-10-12 12:16:11 +0200

    20 sec

--- src/main/java/mods/railcraft/data/recipes/builders/BlastFurnaceRecipeBuilder.java
+++ src/main/java/mods/railcraft/data/recipes/builders/BlastFurnaceRecipeBuilder.java
@@ -14,7 +14,7 @@ import net.minecraft.world.level.ItemLike;
 
 public class BlastFurnaceRecipeBuilder extends AbstractCookingRecipeBuilder {
 
-  public static final int DEFAULT_COOKING_TIME = 16 * 20; // 16 sec
+  public static final int DEFAULT_COOKING_TIME = 20 * 20; // 20 sec
 
   private final int slagOutput;
 
