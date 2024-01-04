e8c8b67bc0ad6db50539340a0e112b9b2de15568
Davide Albiero <davide2910@live.it>
2024-01-04 17:43:28 +0100

    Compile

--- src/main/java/mods/railcraft/data/RailcraftDamageTypeTagsProvider.java
+++ src/main/java/mods/railcraft/data/RailcraftDamageTypeTagsProvider.java
@@ -4,28 +4,17 @@ import java.util.concurrent.CompletableFuture;
 import mods.railcraft.api.core.RailcraftConstants;
 import mods.railcraft.world.damagesource.RailcraftDamageType;
 import net.minecraft.core.HolderLookup;
-import net.minecraft.core.RegistryAccess;
-import net.minecraft.core.RegistrySetBuilder;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
 import net.minecraft.data.PackOutput;
-import net.minecraft.data.tags.TagsProvider;
+import net.minecraft.data.tags.DamageTypeTagsProvider;
 import net.minecraft.tags.DamageTypeTags;
-import net.minecraft.world.damagesource.DamageType;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
-public class RailcraftDamageTypeTagsProvider extends TagsProvider<DamageType> {
-
-  private static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
-      .add(Registries.DAMAGE_TYPE, RailcraftDamageType::bootstrap);
+public class RailcraftDamageTypeTagsProvider extends DamageTypeTagsProvider {
 
   public RailcraftDamageTypeTagsProvider(PackOutput packOutput,
       CompletableFuture<HolderLookup.Provider> lookupProvider,
       ExistingFileHelper existingFileHelper) {
-    super(packOutput, Registries.DAMAGE_TYPE, lookupProvider.thenApply(provider ->
-            BUILDER.buildPatch(RegistryAccess
-                .fromRegistryOfRegistries(BuiltInRegistries.REGISTRY), provider)),
-        RailcraftConstants.ID, existingFileHelper);
+    super(packOutput, lookupProvider, RailcraftConstants.ID, existingFileHelper);
   }
 
   @Override
--- src/test/java/mods/railcraft/ForceTrackEmitterTest.java
+++ src/test/java/mods/railcraft/ForceTrackEmitterTest.java
@@ -5,7 +5,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.gametest.framework.GameTest;
 import net.minecraft.gametest.framework.GameTestHelper;
 import net.minecraft.world.entity.EntityType;
-import net.neoforged.neoforge.common.capabilities.Capabilities;
+import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.gametest.GameTestHolder;
 import net.neoforged.neoforge.gametest.PrefixGameTestTemplate;
 
@@ -15,10 +15,13 @@ public class ForceTrackEmitterTest {
 
   @GameTest(template = "force_track_active", timeoutTicks = 200)
   public static void forceTrackActive(GameTestHelper helper) {
-    var blockEntity = helper.getBlockEntity(new BlockPos(4, 2, 2));
-    helper.onEachTick(() -> blockEntity
-        .getCapability(Capabilities.ENERGY)
-        .ifPresent(x -> x.receiveEnergy(10000, false)));
+    helper.onEachTick(() -> {
+      var energyStorage = helper.getLevel()
+          .getCapability(Capabilities.EnergyStorage.BLOCK, new BlockPos(4, 2, 2), null);
+      if (energyStorage != null) {
+        energyStorage.receiveEnergy(10000, false);
+      }
+    });
 
     helper.pressButton(1, 4, 2);
     helper.succeedWhenEntityPresent(EntityType.MINECART, 10, 3, 2);
