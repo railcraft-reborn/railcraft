e78b4ca6792c749c1b676f167b86e319adb3df89
Davide Albiero <davide2910@live.it>
2024-01-25 17:16:13 +0100

    Use tags, instead of vanilla items Fix #161

--- src/main/java/mods/railcraft/data/recipes/RailcraftRecipeProvider.java
+++ src/main/java/mods/railcraft/data/recipes/RailcraftRecipeProvider.java
@@ -805,9 +805,9 @@ public class RailcraftRecipeProvider extends RecipeProvider {
         .pattern("ace")
         .pattern("adf")
         .define('a', Items.GLASS_PANE)
-        .define('b', Items.LIME_DYE)
-        .define('c', Items.YELLOW_DYE)
-        .define('d', Items.RED_DYE)
+        .define('b', Tags.Items.DYES_LIME)
+        .define('c', Tags.Items.DYES_YELLOW)
+        .define('d', Tags.Items.DYES_RED)
         .define('e', Items.GLOWSTONE_DUST)
         .define('f', Items.REDSTONE)
         .unlockedBy(getHasName(Items.GLOWSTONE_DUST), has(Items.GLOWSTONE_DUST))
@@ -822,7 +822,7 @@ public class RailcraftRecipeProvider extends RecipeProvider {
         .define('a', RailcraftItems.SIGNAL_LAMP.get())
         .define('b', circuit)
         .define('c', Items.IRON_INGOT)
-        .define('d', Items.INK_SAC)
+        .define('d', Tags.Items.DYES_BLACK)
         .unlockedBy(getHasName(circuit), has(circuit))
         .save(finishedRecipe);
   }
@@ -836,7 +836,7 @@ public class RailcraftRecipeProvider extends RecipeProvider {
         .define('a', RailcraftItems.SIGNAL_LAMP.get())
         .define('b', circuit)
         .define('c', Items.IRON_INGOT)
-        .define('d', Items.INK_SAC)
+        .define('d', Tags.Items.DYES_BLACK)
         .define('e', RailcraftItems.RECEIVER_CIRCUIT.get())
         .unlockedBy(getHasName(circuit), has(circuit))
         .save(finishedRecipe);
@@ -886,8 +886,8 @@ public class RailcraftRecipeProvider extends RecipeProvider {
     ShapedRecipeBuilder.shaped(RecipeCategory.MISC, result)
         .pattern("abc")
         .pattern("def")
-        .define('a', Items.RED_DYE)
-        .define('b', Items.INK_SAC)
+        .define('a', Tags.Items.DYES_RED)
+        .define('b', Tags.Items.DYES_BLACK)
         .define('c', Items.BONE_MEAL)
         .define('d', Items.PISTON)
         .define('e', circuit)
@@ -1249,7 +1249,7 @@ public class RailcraftRecipeProvider extends RecipeProvider {
         .pattern("aba")
         .pattern("cdc")
         .pattern("efe")
-        .define('a', Items.YELLOW_DYE)
+        .define('a', Tags.Items.DYES_YELLOW)
         .define('b', Items.REDSTONE_LAMP)
         .define('c', Items.ANVIL)
         .define('d', RailcraftTags.Items.STEEL_BLOCK)
@@ -1262,7 +1262,7 @@ public class RailcraftRecipeProvider extends RecipeProvider {
         .pattern("aba")
         .pattern("cdc")
         .pattern("efe")
-        .define('a', Items.YELLOW_DYE)
+        .define('a', Tags.Items.DYES_YELLOW)
         .define('b', Items.REDSTONE_LAMP)
         .define('c', Items.BLAZE_ROD)
         .define('d', RailcraftTags.Items.STEEL_BLOCK)
@@ -1275,7 +1275,7 @@ public class RailcraftRecipeProvider extends RecipeProvider {
         .pattern("aba")
         .pattern("cdc")
         .pattern("efe")
-        .define('a', Items.YELLOW_DYE)
+        .define('a', Tags.Items.DYES_YELLOW)
         .define('b', Items.REDSTONE_LAMP)
         .define('c', Items.STICKY_PISTON)
         .define('d', RailcraftTags.Items.STEEL_BLOCK)
@@ -1287,7 +1287,7 @@ public class RailcraftRecipeProvider extends RecipeProvider {
         .pattern("aba")
         .pattern("cdc")
         .pattern("efe")
-        .define('a', Items.YELLOW_DYE)
+        .define('a', Tags.Items.DYES_YELLOW)
         .define('b', Items.REDSTONE_LAMP)
         .define('c', Items.PISTON)
         .define('d', RailcraftTags.Items.STEEL_BLOCK)
@@ -1403,11 +1403,11 @@ public class RailcraftRecipeProvider extends RecipeProvider {
         new Tuple<>(Ingredient.of(Tags.Items.DUSTS_REDSTONE), 1)));
     kits(consumer, RailcraftItems.THROTTLE_TRACK_KIT.get(), 4, List.of(
         new Tuple<>(Ingredient.of(Tags.Items.DYES_YELLOW), 1),
-        new Tuple<>(Ingredient.of(Items.INK_SAC), 1),
+        new Tuple<>(Ingredient.of(Tags.Items.DYES_BLACK), 1),
         new Tuple<>(Ingredient.of(Tags.Items.DUSTS_REDSTONE), 1)));
     kits(consumer, RailcraftItems.WHISTLE_TRACK_KIT.get(), 8, List.of(
         new Tuple<>(Ingredient.of(Tags.Items.DYES_YELLOW), 1),
-        new Tuple<>(Ingredient.of(Items.INK_SAC), 1),
+        new Tuple<>(Ingredient.of(Tags.Items.DYES_BLACK), 1),
         new Tuple<>(Ingredient.of(Items.NOTE_BLOCK), 1),
         new Tuple<>(Ingredient.of(Tags.Items.DUSTS_REDSTONE), 1)));
   }
