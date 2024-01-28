abb45e9ad6988e3f937da79aa1317617cdbc31ec
Davide Albiero <davide2910@live.it>
2024-01-28 10:41:54 +0100

    Use DeferredHolder

--- src/main/java/mods/railcraft/world/item/enchantment/RailcraftEnchantments.java
+++ src/main/java/mods/railcraft/world/item/enchantment/RailcraftEnchantments.java
@@ -1,12 +1,12 @@
 package mods.railcraft.world.item.enchantment;
 
-import java.util.function.Supplier;
 import mods.railcraft.api.core.RailcraftConstants;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.monster.Creeper;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 public class RailcraftEnchantments {
@@ -14,22 +14,22 @@ public class RailcraftEnchantments {
   private static final DeferredRegister<Enchantment> deferredRegister =
       DeferredRegister.create(BuiltInRegistries.ENCHANTMENT, RailcraftConstants.ID);
 
-  public static final Supplier<Enchantment> WRECKING =
+  public static final DeferredHolder<Enchantment, RailcraftDamageEnchantment> WRECKING =
       deferredRegister.register("wrecking",
           () -> new RailcraftDamageEnchantment(Enchantment.Rarity.RARE, 1, 11, 20, null, 0.75f,
               EquipmentSlot.MAINHAND));
 
-  public static final Supplier<Enchantment> IMPLOSION =
+  public static final DeferredHolder<Enchantment, RailcraftDamageEnchantment> IMPLOSION =
       deferredRegister.register("implosion",
           () -> new RailcraftDamageEnchantment(Enchantment.Rarity.RARE, 5, 8, 20,
               Creeper.class::isInstance, 2.5f,
               EquipmentSlot.MAINHAND));
 
-  public static final Supplier<DestructionEnchantment> DESTRUCTION =
+  public static final DeferredHolder<Enchantment, DestructionEnchantment> DESTRUCTION =
       deferredRegister.register("destruction",
           () -> new DestructionEnchantment(Enchantment.Rarity.VERY_RARE, EquipmentSlot.MAINHAND));
 
-  public static final Supplier<Enchantment> SMACK =
+  public static final DeferredHolder<Enchantment, SmackEnchantment> SMACK =
       deferredRegister.register("smack",
           () -> new SmackEnchantment(Enchantment.Rarity.VERY_RARE, EquipmentSlot.MAINHAND));
 
