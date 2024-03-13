bcd3e4ec008dfc7e9977abae49820698c0fbf89a
Davide Albiero <davide2910@live.it>
2024-03-13 16:05:34 +0100

    Removed unused EntityDataSerializer

--- src/main/java/mods/railcraft/network/RailcraftDataSerializers.java
+++ src/main/java/mods/railcraft/network/RailcraftDataSerializers.java
@@ -1,6 +1,5 @@
 package mods.railcraft.network;
 
-import java.util.Arrays;
 import java.util.Optional;
 import com.mojang.authlib.GameProfile;
 import mods.railcraft.api.core.RailcraftConstants;
@@ -10,7 +9,6 @@ import mods.railcraft.world.entity.vehicle.locomotive.Locomotive;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.network.syncher.EntityDataSerializer;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -18,42 +16,6 @@ public class RailcraftDataSerializers {
 
   private static final DeferredRegister<EntityDataSerializer<?>> deferredRegister =
       DeferredRegister.create(ForgeRegistries.Keys.ENTITY_DATA_SERIALIZERS, RailcraftConstants.ID);
-
-  public static final EntityDataSerializer<FluidStack> FLUID_STACK =
-      new EntityDataSerializer<>() {
-        @Override
-        public void write(FriendlyByteBuf buf, FluidStack value) {
-          buf.writeFluidStack(value);
-        }
-
-        @Override
-        public FluidStack read(FriendlyByteBuf buf) {
-          return buf.readFluidStack();
-        }
-
-        @Override
-        public FluidStack copy(FluidStack value) {
-          return value.copy();
-        }
-      };
-
-  public static final EntityDataSerializer<byte[]> BYTE_ARRAY =
-      new EntityDataSerializer<>() {
-        @Override
-        public void write(FriendlyByteBuf packetBuffer, byte[] bytes) {
-          packetBuffer.writeByteArray(bytes);
-        }
-
-        @Override
-        public byte[] read(FriendlyByteBuf packetBuffer) {
-          return packetBuffer.readByteArray();
-        }
-
-        @Override
-        public byte[] copy(byte[] value) {
-          return Arrays.copyOf(value, value.length);
-        }
-      };
 
   public static final EntityDataSerializer<Optional<GameProfile>> OPTIONAL_GAME_PROFILE =
       EntityDataSerializer
@@ -75,8 +37,6 @@ public class RailcraftDataSerializers {
       EntityDataSerializer.simpleEnum(Season.class);
 
   public static void register(IEventBus modEventBus) {
-    deferredRegister.register("fluid_stack", () -> FLUID_STACK);
-    deferredRegister.register("byte_array", () -> BYTE_ARRAY);
     deferredRegister.register("optional_game_profile", () -> OPTIONAL_GAME_PROFILE);
     deferredRegister.register("locomotive_mode", () -> LOCOMOTIVE_MODE);
     deferredRegister.register("locomotive_speed", () -> LOCOMOTIVE_SPEED);
