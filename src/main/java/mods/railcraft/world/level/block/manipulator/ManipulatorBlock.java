48ca51ab2d078c3f6fdca6a93ab57ba5f7c354c7
Davide Albiero <davide2910@live.it>
2024-11-12 21:58:42 +0100

    Fix #264

--- src/main/java/mods/railcraft/world/level/block/manipulator/ManipulatorBlock.java
+++ src/main/java/mods/railcraft/world/level/block/manipulator/ManipulatorBlock.java
@@ -7,14 +7,17 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.Containers;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.BaseRailBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.level.redstone.Redstone;
 import net.minecraft.world.phys.BlockHitResult;
 
 public abstract class ManipulatorBlock<T extends ManipulatorBlockEntity> extends BaseEntityBlock {
@@ -53,6 +56,17 @@ public abstract class ManipulatorBlock<T extends ManipulatorBlockEntity> extends
   }
 
   public abstract Direction getFacing(BlockState blockState);
+
+  @Override
+  public int getSignal(BlockState blockState, BlockGetter level, BlockPos blockPos,
+      Direction direction) {
+    boolean emit = false;
+    if (isPowered(blockState)) {
+      var neighborBlockState = level.getBlockState(blockPos.relative(direction.getOpposite()));
+      emit = BaseRailBlock.isRail(neighborBlockState);
+    }
+    return emit ? Redstone.SIGNAL_MAX : Redstone.SIGNAL_NONE;
+  }
 
   @SuppressWarnings("deprecation")
   @Override
