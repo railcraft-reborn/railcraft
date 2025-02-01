e81a6910984e520ca083372759cca9a8774731f1
Davide Albiero <davide2910@live.it>
2025-02-01 19:18:47 +0100

    Fix #255

--- src/main/java/mods/railcraft/world/item/tunnelbore/TunnelBoreHeadItem.java
+++ src/main/java/mods/railcraft/world/item/tunnelbore/TunnelBoreHeadItem.java
@@ -3,9 +3,9 @@ package mods.railcraft.world.item.tunnelbore;
 import java.util.Set;
 import mods.railcraft.api.carts.TunnelBoreHead;
 import mods.railcraft.api.core.RailcraftConstants;
-import mods.railcraft.tags.RailcraftTags;
 import net.minecraft.core.component.DataComponents;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Tier;
 import net.minecraft.world.item.TieredItem;
@@ -20,7 +20,7 @@ public class TunnelBoreHeadItem extends TieredItem implements TunnelBoreHead {
 
   public TunnelBoreHeadItem(Tier tier, String tierName, Properties properties) {
     super(tier, properties.component(DataComponents.TOOL,
-        tier.createToolProperties(RailcraftTags.Blocks.TUNNEL_BORE_MINEABLE_BLOCKS)));
+        tier.createToolProperties(BlockTags.MINEABLE_WITH_PICKAXE)));
     this.textureLocation =
         RailcraftConstants.rl("textures/entity/tunnel_bore/%s_tunnel_bore.png".formatted(tierName));
   }
