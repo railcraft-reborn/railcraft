c6919b186198af32ff840f3e762ec252466c3bd7
Davide Albiero <davide2910@live.it>
2025-08-17 18:49:34 +0200

    Fix #302

--- src/main/java/mods/railcraft/world/level/block/manipulator/CartDispenserBlock.java
+++ src/main/java/mods/railcraft/world/level/block/manipulator/CartDispenserBlock.java
@@ -77,8 +77,7 @@ public class CartDispenserBlock extends ManipulatorBlock<CartDispenserBlockEntit
   @Override
   public void neighborChanged(BlockState state, Level level, BlockPos pos, Block block,
       BlockPos fromPos, boolean isMoving) {
-
-    boolean flag = level.hasNeighborSignal(pos) || level.hasNeighborSignal(pos.above());
+    boolean flag = level.hasNeighborSignal(pos);
     level.setBlock(pos, state.setValue(POWERED, flag), Block.UPDATE_ALL);
 
     if (level.isClientSide()) {
