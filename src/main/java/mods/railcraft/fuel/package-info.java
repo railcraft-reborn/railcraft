a5f3c0fff8a80522246248b314290b6ed78d291d
Davide Albiero <davide2910@live.it>
2025-08-19 10:52:56 +0200

    Delete package-info.java

--- src/main/java/mods/railcraft/fuel/package-info.java
+++
@@ -1,8 +0,0 @@
-@FieldsAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-@ParametersAreNonnullByDefault
-package mods.railcraft.fuel;
-
-import javax.annotation.ParametersAreNonnullByDefault;
-import net.minecraft.FieldsAreNonnullByDefault;
-import net.minecraft.MethodsReturnNonnullByDefault;
