e4550dfb9b0181f84305e4d73edabb1066e21633
Davide Albiero <davide2910@live.it>
2025-11-10 12:42:14 +0100

    Revert "Fix #328"
    This reverts commit 3e0fd847a07be5162c1f9e337292229a2afd55af.

--- src/api/java/mods/railcraft/api/signal/SingleSignalReceiver.java
+++ src/api/java/mods/railcraft/api/signal/SingleSignalReceiver.java
@@ -9,6 +9,7 @@ import mods.railcraft.api.signal.entity.SignalControllerEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.NbtUtils;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraftforge.common.util.INBTSerializable;
@@ -181,7 +182,7 @@ public class SingleSignalReceiver
 
     @Override
     public void deserializeNBT(CompoundTag tag) {
-      if (tag.contains("signalControllerPos")) {
+      if (tag.contains("signalControllerPos", Tag.TAG_COMPOUND)) {
         this.signalControllerPos = NbtUtils.readBlockPos(tag.getCompound("signalControllerPos"));
       }
     }
