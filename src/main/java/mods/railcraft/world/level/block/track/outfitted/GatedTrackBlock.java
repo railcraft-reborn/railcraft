7acb65c933869fa9a994b5530c383894ec03ead0
Davide Albiero <davide2910@live.it>
2026-05-05 18:54:37 +0200

    Fix compilation

--- src/main/java/mods/railcraft/world/level/block/track/outfitted/GatedTrackBlock.java
+++ src/main/java/mods/railcraft/world/level/block/track/outfitted/GatedTrackBlock.java
@@ -42,10 +42,10 @@ public class GatedTrackBlock extends ReversiblePoweredOutfittedTrackBlock {
 
   private static final double MOTION_MIN = 0.2D;
 
-  protected static final VoxelShape Z_SHAPE = Shapes.or(FLAT_AABB, Block.box(0.0D, 0.0D, 6.0D, 16.0D, 16.0D, 10.0D));
-  protected static final VoxelShape X_SHAPE = Shapes.or(FLAT_AABB, Block.box(6.0D, 0.0D, 0.0D, 10.0D, 16.0D, 16.0D));
-  protected static final VoxelShape X_SHAPE_LOW = Shapes.or(FLAT_AABB, Block.box(6.0D, 0.0D, 0.0D, 10.0D, 13.0D, 16.0D));
-  protected static final VoxelShape Z_SHAPE_LOW = Shapes.or(FLAT_AABB, Block.box(0.0D, 0.0D, 6.0D, 16.0D, 13.0D, 10.0D));
+  protected static final VoxelShape Z_SHAPE = Shapes.or(SHAPE_FLAT, Block.box(0.0D, 0.0D, 6.0D, 16.0D, 16.0D, 10.0D));
+  protected static final VoxelShape X_SHAPE = Shapes.or(SHAPE_FLAT, Block.box(6.0D, 0.0D, 0.0D, 10.0D, 16.0D, 16.0D));
+  protected static final VoxelShape X_SHAPE_LOW = Shapes.or(SHAPE_FLAT, Block.box(6.0D, 0.0D, 0.0D, 10.0D, 13.0D, 16.0D));
+  protected static final VoxelShape Z_SHAPE_LOW = Shapes.or(SHAPE_FLAT, Block.box(0.0D, 0.0D, 6.0D, 16.0D, 13.0D, 10.0D));
   protected static final VoxelShape Z_COLLISION_SHAPE =
       box(0.0D, 0.0D, 6.0D, 16.0D, 24.0D, 10.0D);
   protected static final VoxelShape X_COLLISION_SHAPE =
